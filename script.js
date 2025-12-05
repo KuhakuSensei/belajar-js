37164612046520d38dcb7fa1b24b3f567ac7e213
Nizar <nizar.ardany72@gmail.com>
2025-12-06 00:25:50 +0700

    feat: tambah kalkulator sederhana + tombol reset

--- script.js
+++ script.js
@@ -1,12 +1,28 @@
-const tombolA = document.getElementById("tombolA");
-const tombolB = document.getElementById("tombolB");
-const teks = document.getElementById("teks");
+const angka1 = document.getElementById("angka1");
+const angka2 = document.getElementById("angka2");
+const hasil = document.getElementById("hasil");
 
-tombolA.addEventListener("click", function () {
-  teks.textContent = "Halo, saya belajar JavaScript!";
+document.getElementById("tambah").addEventListener("click", function () {
+  hasil.textContent = Number(angka1.value) + Number(angka2.value);
 });
 
-tombolB.addEventListener("click", function () {
-  teks.textContent = "Saya semangat latihan!";
+document.getElementById("kurang").addEventListener("click", function () {
+  hasil.textContent = Number(angka1.value) - Number(angka2.value);
 });
-console.log("Latihan Git nomor 3");
+
+document.getElementById("kali").addEventListener("click", function () {
+  hasil.textContent = Number(angka1.value) * Number(angka2.value);
+});
+
+document.getElementById("bagi").addEventListener("click", function () {
+  if (Number(angka2.value) === 0) {
+    hasil.textContent = "Tidak bisa membagi 0";
+  } else {
+    hasil.textContent = Number(angka1.value) / Number(angka2.value);
+  }
+});
+document.getElementById("reset").addEventListener("click", function () {
+  angka1.value = "";
+  angka2.value = "";
+  hasil.textContent = "0";
+});
